40a4c21beddb9bc1860fafa64cbec06a07f122bd
nawangandrian <nawangandrian@gmail.com>
2024-04-27 22:22:51 +0700

    First Commit

---
+++ practice.py
@@ -0,0 +1,9 @@
+import requests
+
+api_key = 'd25c522e-0386-426e-b3e4-9a275531173e'
+word = 'voluminous'
+root_url = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json'
+final_url = f'{root_url}/{word}?key={api_key}'
+r = requests.get(final_url)
+result = r.json()
+print(result)
